e9a16979b60506d5b790bb5f81f48da7fe87969d
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-10-27 14:52:29 +0300

    96br

--- sign-up.js
+++ sign-up.js
@@ -66,7 +66,7 @@ const onSubmit = async (event) => {
     const { ip } = await getIP();
 
     const body = JSON.stringify({
-      name: "slotsbet",
+      name: "96br",
       email: formRef.email.value,
       browserId,
       ip,
